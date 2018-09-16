efe3c8b04bafd4c279bbb8bf240409eda0c83de0
AloyDev <tics_zz@hotmail.com>
2018-09-16 15:20:52 +0800

    1. Creation of WCF Service 2. Creation of Hosting for the WCF Service; Hosting on Console Application, IIS and WCF Service Library.

---
+++ PizzaServices/IPizzaService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace PizzaServices
+{
+    [ServiceContract]
+    public interface IPizzaService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+
+        [OperationContract()]
+        List<Customer> GetCustomers();
+
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
---
+++ PizzaServices/PizzaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Zza.Data;
+using Zza.Entities;
+
+namespace PizzaServices
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class PizzaService : IPizzaService, IDisposable
+    {
+        readonly ZzaDbContext _Context = new ZzaDbContext();
+
+        public List<Product> GetProducts()
+        {
+            return _Context.Products.ToList();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _Context.Customers.ToList();
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void SubmitOrder(Order order)
+        {
+            _Context.Orders.Add(order);
+            order.OrderItems.ForEach(oi => _Context.OrderItems.Add(oi));
+            _Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            // Dispose the DB connection 
+            // WCF automatically dispose the dbContext of the service that it is hosting, if it implements IDisposable
+            _Context.Dispose();
+        }
+    }
+}
