227420a9f2c86ee475f0f294dca4d19fcbda5ee8
AloyDev <tics_zz@hotmail.com>
2018-09-16 18:15:13 +0800

    Added Client side implementation to call WCF Service

---
+++ Zza.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Client.PizzaServices;
+
+namespace Zza.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaServiceClient proxy = new PizzaServiceClient("BasicHttpBinding_IPizzaService");
+            var products = proxy.GetProducts();
+            var customers = proxy.GetCustomers();
+            proxy.Close();
+           
+        }
+    }
+}
